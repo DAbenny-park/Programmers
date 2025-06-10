2b63fca627db6d435f3e30bac0a6c948be161354
benny <107995643+DAbenny-park@users.noreply.github.com>
2025-06-10 23:26:56 +0900

    Delete 프로그래머스/lv1/12918. 문자열 다루기 기본 directory

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    if (len(s) == 4 or len(s) == 6) and s.isdigit():
-        return True
-    else:
-        return False
-  
-# 2
-def solution(s):
-    return s.isdigit() and len(s) in (4,6)
